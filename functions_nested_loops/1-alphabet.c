8b44508ecab3a562a2ac740d4665c5bce921e1e5
Haifahasher <Haifahasher@gmail.com>
2025-05-04 18:31:21 +0000

    update task 1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,14 +1,14 @@
-#include <stdio.h>
 #include "main.h"
+
 /**
-* print_alphabet - prints the lowercase alphabet followed by a newline
-* Return: void
+* print_alphabet - prints the lowercase alphabet, followed by a newline
 */
 void print_alphabet(void)
 {
 int i;
 
 for (i = 'a'; i <= 'z'; i++)
-putchar(i);
-putchar('\n');
+_putchar(i);
+
+_putchar('\n');
 }
