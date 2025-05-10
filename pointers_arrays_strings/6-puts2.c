110d9b8ca6b60480e859b2aa2e1c115b5a8a6974
Haifahasher <Haifahasher@gmail.com>
2025-05-10 09:10:04 +0000

    update and add tasks

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,17 +1,37 @@
 #include "main.h"
+/**
+ * _strlen - count the number of characters in a string
+ * @s: pointer to the input string
+ * Return: length of the string (excluding the null terminator)
+ */
+int _strlen(char *s)
+{
+int i = 0, length = 0;
+
+while (s[i++])
+{
+length++;
+}
+
+return (length);
+}
 
 /**
  * puts2 - prints every other character of a string, starting with the first
+ *         character, followed by a new line.
  * @str: pointer to the input string
  */
 void puts2(char *str)
 {
-int i = 0;
+int i, length;
 
-while (str[i])
+length = _strlen(str);
+for (i = 0; i <= (length - 1); i++)
+{
+if ((i % 2) == 0)
 {
 _putchar(str[i]);
-i += 2;
+}
 }
 _putchar('\n');
 }
---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * print - outputs a long integer to stdout using _putchar
+ * @n: the number to print
+ */
+
+void print(long n)
+{
+if (n < 0)
+{
+_putchar('-');
+n = -n;
+}
+
+if (n / 10)
+print(n / 10);
+_putchar(n % 10 + '0');
+}
+
+void print_array(int *a, int n)
+{
+int i;
+
+for (i = 0; i <= (n - 1); i++)
+{
+if (i < (n - 1))
+{
+print(a[i]);
+_putchar(',');
+_putchar(' ');
+}
+else
+{
+print(a[i]);
+}
+}
+_putchar('\n');
+}
