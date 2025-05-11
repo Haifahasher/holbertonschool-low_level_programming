c222338d7ac175481a4f70dbc86784f4e61114fe
Haifahasher <Haifahasher@gmail.com>
2025-05-11 13:45:55 +0000

    betty style

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -4,6 +4,11 @@
  * _strcmp - compares two strings
  * @s1: pointer to the first string
  * @s2: poi
+ *  * Return: If str1 < str2, the negative difference of the first unmatched
+ *         characters.
+ *         If str1 == str2, 0.
+ *         If str1 > str2, the positive difference of the first unmatched
+ *         characters.
  */
 int _strcmp(char *s1, char *s2)
 {
