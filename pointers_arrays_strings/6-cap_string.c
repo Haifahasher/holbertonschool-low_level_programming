afb717e10562b61d0703a1825c928289e3c2c01a
Haifahasher <Haifahasher@gmail.com>
2025-05-13 12:43:09 +0000

    add update task 6

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -2,37 +2,39 @@
 
 /**
  * cap_string - capitalizes all words of a string
- * @s: pointer to the string to modify
+ * @str: pointer to the string to modify
  * Return: pointer to the modified string
  */
-char *cap_string(char *s)
+
+
+
+char *cap_string(char *str)
 {
 int i = 0;
-int capitalize = 1;
 
-while (s[i])
+while (str[i])
 {
-if (capitalize && s[i] >= 'a' && s[i] <= 'z')
-{
-s[i] = s[i] - ('a' - 'A');
-capitalize = 0;
-}
-else if (!(s[i] >= 'A' && s[i] <= 'Z'))
-{
-if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n' ||
-s[i] == ',' || s[i] == ';' || s[i] == '.' ||
-s[i] == '!' || s[i] == '?' || s[i] == '"' ||
-s[i] == '(' || s[i] == ')' || s[i] == '{' ||
-s[i] == '}')
-{
-capitalize = 1;
-}
-}
-else
-{
-capitalize = 0;
-}
+while (!(str[i] >= 'a' && str[i] <= 'z'))
+i++;
+
+if (str[i - 1] == ' ' ||
+str[i - 1] == '\t' ||
+str[i - 1] == '\n' ||
+str[i - 1] == ',' ||
+str[i - 1] == ';' ||
+str[i - 1] == '.' ||
+str[i - 1] == '!' ||
+str[i - 1] == '?' ||
+str[i - 1] == '"' ||
+str[i - 1] == '(' ||
+str[i - 1] == ')' ||
+str[i - 1] == '{' ||
+str[i - 1] == '}' ||
+i == 0)
+str[i] -= 32;
+
 i++;
 }
-return (s);
+
+return (str);
 }
