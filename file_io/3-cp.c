afd3006d8fd68d05cccdb81e80cc32c366e1a9a6
Haifahasher <Haifahasher@gmail.com>
2025-06-14 16:28:59 +0000

    update task 3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,5 +1,7 @@
 #include "main.h"
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 /**
  * print_error - Prints an error message to STDERR and exits.
  * @exit_code: Exit code to use.
